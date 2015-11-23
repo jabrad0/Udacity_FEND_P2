2812bdaabc71b7b4a08f3ef7e849f770c57e7dda
jabrad0 <julibrady@gmail.com>
2015-11-23 09:33:53 -0600

    Map infoWindow code updated to have infoWindow close when another one opens.

--- js/helper.js
+++ js/helper.js
@@ -2,26 +2,17 @@ var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLlinkedin = '<li class="flex-item"><span class="orange-text">linkedin</span>';
+var HTMLlinkedin = '<li class="flex-item"><span class="orange-text">linkedin:</span>';
 var HTMLlinkedinURL = '<span class="white-text"><a href="#" target="_blank">%data%</a></span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email:</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter:</span>';
 var HTMLtwitterURL = '<span class="white-text"><a href="#" target="_blank">%data%</a></span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github:</span>';
 var HTMLgithubURL = '<span class="white-text"><a href="#" target="_blank">%data%</a></span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span>';
 var HTMLlocationURL = '<span class="white-text"><a href="#" target="_blank">%data%</a></span></li>';
 
-// var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
-
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-
-
-
-
-
-
-
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
@@ -63,12 +54,12 @@ var googleMap = '<div id="map"></div>';
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName(name) || function(){};
-    $('#name').html(iName);
-  });
-});
+// $(document).ready(function() {
+//   $('button').click(function() {
+//     var iName = inName(name) || function(){};
+//     $('#name').html(iName);
+//   });
+// });
 
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
@@ -92,7 +83,6 @@ $(document).click(function(loc) {
   logClicks(x,y);
 });
 
-
 // Google Map
 // documentation: //https://developers.google.com/maps/documentation/javascript/reference
 
@@ -101,42 +91,36 @@ var map;    // declares a global map variable
 Start here! initializeMap() is called when page is loaded.
 */
 function initializeMap() {
-
   var locations;
-
   var mapOptions = {
-    disableDefaultUI: true
+    disableDefaultUI: false,
+    mapTypeId: google.maps.MapTypeId.TERRAIN,
+    heading: 45
   };
 
-  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
-  // <div id="map">, which is appended as part of an exercise late in the course.
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  //***create InfoWindow in main initializeMap() function
+  //then inside createMapMarker create click event on marker
+  //to open infoWindow -this allows for the infowindows to close when you
+  //click on another one - I have no idea why this works right now.***
+  var infoWindow = new google.maps.InfoWindow({
+    // content: applied in clickevent in createMapMarker()
+  });
 
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
   /*
   locationFinder() returns an array of every location string from the JSONs
   written for bio, education, and work.
   */
   function locationFinder() {
-
-    // initializes an empty array
     var locations = [];
-
-    // adds the single location property from bio to the locations array
     locations.push(bio.location);
-
-    // iterates through school locations and appends each location to
-    // the locations array
     for (var school in education.schools) {
       locations.push(education.schools[school].location);
     }
-
-    // iterates through work locations and appends each location to
-    // the locations array
     for (var job in work.jobs) {
       locations.push(work.jobs[job].location);
     }
-
     return locations;
   }
 
@@ -146,8 +130,6 @@ function initializeMap() {
   about a single location.
   */
   function createMapMarker(placeData) {
-
-    // The next lines save location data from the search result object to local variables
     var lat = placeData.geometry.location.lat();  // latitude from the place service
     var lon = placeData.geometry.location.lng();  // longitude from the place service
     var name = placeData.formatted_address;   // name of the place from the place service
@@ -155,22 +137,15 @@ function initializeMap() {
 
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
-      map: map,
+      map: map, //specifies on which map to place marker
       position: placeData.geometry.location,
       title: name
     });
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(name);
+      infoWindow.open(map, this); //'this' or 'marker' places pin at proper anchor
     });
-
-    // hmmmm, I wonder what this is about...
-    //google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
-    //});
 
     // this is where the pin actually gets added to the map.
     // bounds.extend() takes in a map location object
@@ -200,15 +175,11 @@ function initializeMap() {
     // creates a Google place search service object. PlacesService does the work of
     // actually searching for location data.
     var service = new google.maps.places.PlacesService(map);
-
-    // Iterates through the array of locations, creates a search object for each location
     for (var place in locations) {
-
       // the search request object
       var request = {
         query: locations[place]
       };
-
       // Actually searches the Google Maps API for location data and runs the callback
       // function with the search results after each search.
       service.textSearch(request, callback);
@@ -216,6 +187,7 @@ function initializeMap() {
   }
 
   // Sets the boundaries of the map based on pin locations
+  //LatLngBounds() is a maps method
   window.mapBounds = new google.maps.LatLngBounds();
 
   // locations is an array of location strings returned from locationFinder()
@@ -225,11 +197,7 @@ function initializeMap() {
   // the locations array
   pinPoster(locations);
 
-}
-
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
+}//this is the end of the initializeMap() function
 
 // Calls the initializeMap() function when the page loads
 window.addEventListener('load', initializeMap);
